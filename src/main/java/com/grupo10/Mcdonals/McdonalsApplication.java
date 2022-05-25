50c151a5698576ee00cef1248c7267185a830352
LeonelSpinazzola <sadpepe9j11@gmail.com>
2022-05-25 20:32:14 -0300

    Inicio del proyecto
    inicio y configuracion default

---
+++ src/main/java/com/grupo10/Mcdonals/McdonalsApplication.java
@@ -0,0 +1,13 @@
+package com.grupo10.Mcdonals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McdonalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McdonalsApplication.class, args);
+	}
+
+}
